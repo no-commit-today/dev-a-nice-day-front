9d6a2d599719e1a4c7b892d4280c7a602df64f1b
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-06-09 10:35:07 +0900

    feat/ 현재 path에 맞게 UI 변경

--- src/app/_components/BottomTabBar.tsx
+++ src/app/_components/BottomTabBar.tsx
@@ -1,8 +1,10 @@
+"use client";
 import Link from "next/link";
 import styles from "./BottomTabBar.module.css";
-import Description from "@/../public/assets/description.svg";
+import { usePathname } from "next/navigation";
 
 const BottomTabBar = () => {
+  const pathname = usePathname();
   return (
     <div className={styles.container}>
       <Link href="/" className={styles.tabBtn}>
@@ -18,8 +20,8 @@ const BottomTabBar = () => {
               <g clip-path="url(#clip0_147_1585)">
                 <path
                   d="M11.0625 2.37302H4.8125C3.95312 2.37302 3.25781 3.07614 3.25781 3.93552L3.25 16.4355C3.25 17.2949 3.94531 17.998 4.80469 17.998H14.1875C15.0469 17.998 15.75 17.2949 15.75 16.4355V7.06052L11.0625 2.37302ZM12.625 14.873H6.375V13.3105H12.625V14.873ZM12.625 11.748H6.375V10.1855H12.625V11.748ZM10.2812 7.84177V3.54489L14.5781 7.84177H10.2812Z"
-                  fill="#898989"
-                  fill-opacity="0.6"
+                  fill={pathname === "/" ? "#fff" : "#898989"}
+                  fill-opacity={pathname === "/" ? "1" : "0.6"}
                 />
               </g>
               <defs>
@@ -34,7 +36,9 @@ const BottomTabBar = () => {
               </defs>
             </svg>
           </div>
-          <span className={styles.text}>홈</span>
+          <span className={pathname === "/" ? styles.textFocused : styles.text}>
+            홈
+          </span>
         </div>
       </Link>
       <Link href="/content" className={styles.tabBtn}>
@@ -50,8 +54,8 @@ const BottomTabBar = () => {
               <g clip-path="url(#clip0_147_1585)">
                 <path
                   d="M11.0625 2.37302H4.8125C3.95312 2.37302 3.25781 3.07614 3.25781 3.93552L3.25 16.4355C3.25 17.2949 3.94531 17.998 4.80469 17.998H14.1875C15.0469 17.998 15.75 17.2949 15.75 16.4355V7.06052L11.0625 2.37302ZM12.625 14.873H6.375V13.3105H12.625V14.873ZM12.625 11.748H6.375V10.1855H12.625V11.748ZM10.2812 7.84177V3.54489L14.5781 7.84177H10.2812Z"
-                  fill="#898989"
-                  fill-opacity="0.6"
+                  fill={pathname === "/content" ? "#fff" : "#898989"}
+                  fill-opacity={pathname === "/content" ? "1" : "0.6"}
                 />
               </g>
               <defs>
@@ -66,7 +70,13 @@ const BottomTabBar = () => {
               </defs>
             </svg>
           </div>
-          <span className={styles.text}>콘텐츠</span>
+          <span
+            className={
+              pathname === "/content" ? styles.textFocused : styles.text
+            }
+          >
+            콘텐츠
+          </span>
         </div>
       </Link>
       <Link href="/setting" className={styles.tabBtn}>
@@ -82,8 +92,8 @@ const BottomTabBar = () => {
               <g clip-path="url(#clip0_147_1585)">
                 <path
                   d="M11.0625 2.37302H4.8125C3.95312 2.37302 3.25781 3.07614 3.25781 3.93552L3.25 16.4355C3.25 17.2949 3.94531 17.998 4.80469 17.998H14.1875C15.0469 17.998 15.75 17.2949 15.75 16.4355V7.06052L11.0625 2.37302ZM12.625 14.873H6.375V13.3105H12.625V14.873ZM12.625 11.748H6.375V10.1855H12.625V11.748ZM10.2812 7.84177V3.54489L14.5781 7.84177H10.2812Z"
-                  fill="#898989"
-                  fill-opacity="0.6"
+                  fill={pathname === "/setting" ? "#fff" : "#898989"}
+                  fill-opacity={pathname === "/setting" ? "1" : "0.6"}
                 />
               </g>
               <defs>
@@ -98,7 +108,13 @@ const BottomTabBar = () => {
               </defs>
             </svg>
           </div>
-          <span className={styles.text}>설정</span>
+          <span
+            className={
+              pathname === "/setting" ? styles.textFocused : styles.text
+            }
+          >
+            설정
+          </span>
         </div>
       </Link>
     </div>
