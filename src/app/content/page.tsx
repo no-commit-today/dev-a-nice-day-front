80a6984d265066ab119e460894ed69806ccf9835
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-06-09 10:37:45 +0900

    feat/ content, setting css 적용

--- src/app/content/page.tsx
+++ src/app/content/page.tsx
@@ -1,5 +1,7 @@
+import styles from "./page.module.css";
+
 const Content = () => {
-  return <div>Content</div>;
+  return <div className={styles.container}>Content</div>;
 };
 
 export default Content;
--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -1,5 +1,7 @@
+import styles from "./page.module.css";
+
 const Setting = () => {
-  return <div>Setting</div>;
+  return <div className={styles.container}>Setting</div>;
 };
 
 export default Setting;
