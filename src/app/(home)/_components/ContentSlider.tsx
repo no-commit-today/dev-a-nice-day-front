f115cd3301b8f12474537171c2818fa89159416e
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-06-23 21:10:35 +0900

    feat/ 슬라이더 컴포넌트 제작
    - 일단 오버플로 스크롤

---
+++ src/app/(home)/_components/ContentSlider.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import styles from "./ContentSlider.module.css";
+import "swiper/css";
+import IndexIndicator from "./IndexIndicator";
+
+export default function ContentSlider({ contentData }: { contentData: any[] }) {
+  return (
+    <div className="swiper-container">
+      <Swiper>
+        {contentData.map((content) => {
+          const summaryArray = content.summary
+            .split("-")
+            .map((item: string) => item.trim())
+            .filter((item: string) => item);
+
+          return (
+            <SwiperSlide key={content.id}>
+              <div className={styles.slideContainer}>
+                <h1 className={styles.dateText}>{content.publishedDate}</h1>
+                <div className={styles.titleBox}>
+                  <img
+                    src={content.providerIconUrl}
+                    width={30}
+                    height={30}
+                    style={{ borderRadius: 7 }}
+                  ></img>
+                  <div className={styles.titleWrap}>
+                    <h2 className={styles.providerTitle}>
+                      {content.providerTitle}
+                    </h2>
+                    <h2 className={styles.title}>{content.title}</h2>
+                  </div>
+                </div>
+                <div className={styles.summaryBox}>
+                  <img
+                    src={content.imageUrl}
+                    width={"100%"}
+                    height={180}
+                    style={{
+                      objectFit: "cover",
+                      borderTopLeftRadius: 10,
+                      borderTopRightRadius: 10,
+                    }}
+                  ></img>
+                  <div className={styles.summary}>
+                    {summaryArray.map((summary: string, index: number) => {
+                      return (
+                        <div className={styles.summaryTextBox}>
+                          <IndexIndicator index={index} />
+                          <h2 className={styles.summaryText}>{summary}</h2>
+                        </div>
+                      );
+                    })}
+                  </div>
+                  <div className={styles.categoryBox}>
+                    {content.categories.map((category: string) => {
+                      return (
+                        <h2 className={styles.categoryText}>{category}</h2>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+}
