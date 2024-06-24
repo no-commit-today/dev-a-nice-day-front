8faff1700b8655e756fcb9f6e12b307136584bed
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-06-24 14:58:33 +0900

    feat/ 설정 UI 구현

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -1,5 +1,17 @@
 import styles from "./page.module.css";
+import carret_right from "@/../public/assets/carret_right.svg";
 
 export default function Setting() {
-  return <div className={styles.container}>Setting</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.termsBox}>
+        <h1 className={styles.termsText}>서비스 이용 약관</h1>
+        <img src={carret_right.src} />
+      </div>
+      <div className={styles.termsBox}>
+        <h1 className={styles.termsText}>개인정보 처리 방침</h1>
+        <img src={carret_right.src} />
+      </div>
+    </div>
+  );
 }
