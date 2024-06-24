f7f0d05b2e632e5b6982d90448e3a06ac938b6e2
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-06-24 15:14:55 +0900

    fix/ 설정 페이지 UI 정렬 수정

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -4,13 +4,15 @@ import carret_right from "@/../public/assets/carret_right.svg";
 export default function Setting() {
   return (
     <div className={styles.container}>
-      <div className={styles.termsBox}>
-        <h1 className={styles.termsText}>서비스 이용 약관</h1>
-        <img src={carret_right.src} />
-      </div>
-      <div className={styles.termsBox}>
-        <h1 className={styles.termsText}>개인정보 처리 방침</h1>
-        <img src={carret_right.src} />
+      <div className={styles.contentBox}>
+        <div className={styles.termsBox}>
+          <h1 className={styles.termsText}>서비스 이용 약관</h1>
+          <img src={carret_right.src} />
+        </div>
+        <div className={styles.termsBox}>
+          <h1 className={styles.termsText}>개인정보 처리 방침</h1>
+          <img src={carret_right.src} />
+        </div>
       </div>
     </div>
   );
