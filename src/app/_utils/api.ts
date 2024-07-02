285c05c1a8d7e667c1ff759ee09f3c9ddf0b2497
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-07-02 19:55:59 +0900

    feat/ api에 셔플 로직 추가

--- src/app/_utils/api.ts
+++ src/app/_utils/api.ts
@@ -1,10 +1,25 @@
 const BASE_URL = "https://2a1a5a16-d662-403d-9785-7430252af234.mock.pstmn.io";
 
-const getContent = async (page: number, searchParams: string) => {
+const contentFetch = async (page: number, searchParams: string) => {
   const data = await fetch(
     `${BASE_URL}/content?page=${page}&size=10&${searchParams}`
   );
   return data.json();
+};
+
+function shuffleArray(array: object[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+const getContent = async (page: number, searchParams: string) => {
+  const data = await contentFetch(page, searchParams);
+  shuffleArray(data.content);
+
+  return data;
 };
 
 export { getContent };
