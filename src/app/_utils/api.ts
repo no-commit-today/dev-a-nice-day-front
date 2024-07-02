018c3b1afc8015440d16d16320144ca28f1db95c
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-07-02 17:30:57 +0900

    feat/ api에 카테고리 추가

--- src/app/_utils/api.ts
+++ src/app/_utils/api.ts
@@ -1,7 +1,9 @@
 const BASE_URL = "https://2a1a5a16-d662-403d-9785-7430252af234.mock.pstmn.io";
 
-const getContent = async (page: number) => {
-  const data = await fetch(`${BASE_URL}/content?page=${page}&size=10`);
+const getContent = async (page: number, searchParams: string) => {
+  const data = await fetch(
+    `${BASE_URL}/content?page=${page}&size=10&${searchParams}`
+  );
   return data.json();
 };
 
