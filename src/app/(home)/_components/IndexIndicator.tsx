a042929ba528bf6038dae745d010ffaee1735cdb
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-07-04 22:01:43 +0900

    feat/ 요약 번호 추가

--- src/app/(home)/_components/IndexIndicator.tsx
+++ src/app/(home)/_components/IndexIndicator.tsx
@@ -1,6 +1,8 @@
 import Number1 from "@/../public/assets/number_1.svg";
 import Number2 from "@/../public/assets/number_2.svg";
 import Number3 from "@/../public/assets/number_3.svg";
+import Number4 from "@/../public/assets/number_4.svg";
+import Number5 from "@/../public/assets/number_5.svg";
 
 export default function IndexIndicator({ index }: { index: number }) {
   if (index === 0) {
@@ -8,7 +10,13 @@ export default function IndexIndicator({ index }: { index: number }) {
   }
   if (index === 1) {
     return <img src={Number2.src} width={23} height={23} />;
-  } else {
+  }
+  if (index === 2) {
     return <img src={Number3.src} width={23} height={23} />;
+  }
+  if (index === 3) {
+    return <img src={Number4.src} width={23} height={23} />;
+  } else {
+    return <img src={Number5.src} width={23} height={23} />;
   }
 }
