fc718d3bf41c15a80a01d7c282ba22407d218b94
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-07-14 13:06:00 +0900

    refactor/ content slider 네이밍 변경

--- src/app/(home)/_components/ContentSlider.tsx
+++ src/app/(home)/_components/ContentSlider.tsx
@@ -7,7 +7,11 @@ import IndexIndicator from "./IndexIndicator";
 import Image from "next/image";
 import { Categories } from "@/app/_components/Categories";
 import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
-import { BASE_URL, getContentsCount, getShuffledContents } from "@/app/_utils/api";
+import {
+  BASE_URL,
+  getContentsCount,
+  getShuffledContents,
+} from "@/app/_utils/api";
 import { useEffect, useRef, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { Mousewheel } from "swiper/modules";
@@ -19,31 +23,29 @@ export default function ContentSlider() {
   const goToLink = ({ url }: { url: string }) => {
     window.open(url);
   };
-  const { data: totalPageData } = useQuery({
-    queryKey: ["totalPageData", searchParams],
+  const { data: contentsCountData } = useQuery({
+    queryKey: ["contentsCountData", searchParams],
     queryFn: () => getContentsCount(searchParams),
     staleTime: 5 * 1000 * 60,
     gcTime: 30 * 1000 * 60,
   });
   const getRandomNumber = (allPageParams: number[]) => {
-    var pageCount = 50;
-    if (totalPageData) {
-      pageCount = totalPageData.count;
+    let pagesCount = 50;
+    if (contentsCountData) {
+      pagesCount = Math.ceil(contentsCountData.count / 10);
     }
     // 전체 페이지 개수를 통해 전체 페이지 배열 생성
-    const totalNumber = Math.ceil(pageCount / 10);
-    var totalPageArray = Array.from({ length: totalNumber }, (_, i) => i + 1);
+    let totalPageArray = Array.from({ length: pagesCount }, (_, i) => i + 1);
 
     // 지금까지 받아왔던 페이지들 배열에서 삭제 (모두 한번씩 다 받아온 경우 그냥 랜덤으로 뿌림)
-    if (allPageParams.length < totalNumber) {
+    if (allPageParams.length < pagesCount) {
       totalPageArray = totalPageArray.filter((n) => !allPageParams.includes(n));
     }
 
     // 랜덤 인덱스 선택
     const randomIndex = Math.floor(Math.random() * totalPageArray.length);
-    const selectedNumber = totalPageArray[randomIndex];
 
-    return selectedNumber;
+    return totalPageArray[randomIndex];
   };
 
   const {
@@ -59,7 +61,7 @@ export default function ContentSlider() {
     getNextPageParam: (lastPage, allPages, lastPageParam, allPageParams) => {
       return getRandomNumber(allPageParams);
     },
-    enabled: totalPageData !== undefined,
+    enabled: contentsCountData !== undefined,
     staleTime: 5 * 1000 * 60,
     gcTime: 30 * 1000 * 60,
   });
@@ -70,7 +72,7 @@ export default function ContentSlider() {
   // 데이터 추가 요청
   const pushMore = async () => {
     if (!isFetchingNextPage) {
-      fetchNextPage();
+      await fetchNextPage();
     }
   };
 
