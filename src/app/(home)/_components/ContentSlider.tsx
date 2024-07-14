c89ee8cf6a22b2d3c6ee4eccfd3e1182c0d7ea58
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-07-14 14:30:27 +0900

    refactor/ 쓸모없는 변수 삭제

--- src/app/(home)/_components/ContentSlider.tsx
+++ src/app/(home)/_components/ContentSlider.tsx
@@ -29,23 +29,28 @@ export default function ContentSlider() {
     staleTime: 5 * 1000 * 60,
     gcTime: 30 * 1000 * 60,
   });
+
   const getRandomNumber = (allPageParams: number[]) => {
-    let pagesCount = 50;
     if (contentsCountData) {
-      pagesCount = Math.ceil(contentsCountData.count / 10);
+      // 전체 콘텐츠 개수를 통해 전체 페이지 개수를 계산
+      let pagesCount = Math.ceil(contentsCountData.count / 10);
+
+      // 전체 페이지 개수를 통해 전체 페이지 배열 생성
+      let totalPageArray = Array.from({ length: pagesCount }, (_, i) => i + 1);
+
+      // 지금까지 받아왔던 페이지들 배열에서 삭제 (모두 한번씩 다 받아온 경우 그냥 랜덤으로 뿌림)
+      if (allPageParams.length < pagesCount) {
+        totalPageArray = totalPageArray.filter(
+          (n) => !allPageParams.includes(n)
+        );
+      }
+
+      // 랜덤 인덱스 선택
+      const randomIndex = Math.floor(Math.random() * totalPageArray.length);
+
+      return totalPageArray[randomIndex];
     }
-    // 전체 페이지 개수를 통해 전체 페이지 배열 생성
-    let totalPageArray = Array.from({ length: pagesCount }, (_, i) => i + 1);
-
-    // 지금까지 받아왔던 페이지들 배열에서 삭제 (모두 한번씩 다 받아온 경우 그냥 랜덤으로 뿌림)
-    if (allPageParams.length < pagesCount) {
-      totalPageArray = totalPageArray.filter((n) => !allPageParams.includes(n));
-    }
-
-    // 랜덤 인덱스 선택
-    const randomIndex = Math.floor(Math.random() * totalPageArray.length);
-
-    return totalPageArray[randomIndex];
+    return 0;
   };
 
   const {
