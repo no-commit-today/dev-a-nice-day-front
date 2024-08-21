f224105e08c079b7864df4070641b05dd5716ea2
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-08-21 16:32:25 +0900

    feat: 리액트 쿼리 ssr 적용

--- src/app/(home)/_components/ContentSlider.tsx
+++ src/app/(home)/_components/ContentSlider.tsx
@@ -17,36 +17,20 @@ import { Mousewheel } from "swiper/modules";
 import useParams from "@/app/_hooks/useParams";
 import { IContentData } from "@/app";
 import no_image from "@/../public/assets/no_image.svg";
+import getRandomNumber from "@/app/_utils/getRandomNumber";
 
-export default function ContentSlider() {
+export default function ContentSlider({
+  initialData,
+  contentsCountData,
+}: {
+  initialData: { pages: { content: IContentData[] }[]; pageParams: number[] };
+  contentsCountData: { count: number };
+}) {
   // 몇페이지 전에 패치할 것인지.
   const pagesBeforeFetch = 3;
   const searchParams = useParams("categories").getParamsToString();
   const [contentsData, setContentsData] = useState<IContentData[] | null>(null);
   const [scrollPosition, setScrollPosition] = useState(0);
-
-  const getRandomNumber = (allPageParams: number[]) => {
-    if (contentsCountData) {
-      // 전체 콘텐츠 개수를 통해 전체 페이지 개수를 계산
-      let pagesCount = Math.ceil(contentsCountData.count / 10);
-
-      // 전체 페이지 개수를 통해 전체 페이지 배열 생성
-      let totalPageArray = Array.from({ length: pagesCount }, (_, i) => i + 1);
-
-      // 지금까지 받아왔던 페이지들 배열에서 삭제 (모두 한번씩 다 받아온 경우 그냥 랜덤으로 뿌림)
-      if (allPageParams.length < pagesCount) {
-        totalPageArray = totalPageArray.filter(
-          (n) => !allPageParams.includes(n)
-        );
-      }
-
-      // 랜덤 인덱스 선택
-      const randomIndex = Math.floor(Math.random() * totalPageArray.length);
-
-      return totalPageArray[randomIndex];
-    }
-    return 1;
-  };
 
   const goToLink = ({ url }: { url: string }) => {
     window.open(url);
@@ -59,13 +43,6 @@ export default function ContentSlider() {
     }
   };
 
-  const { data: contentsCountData } = useQuery({
-    queryKey: ["contentsCountData", searchParams],
-    queryFn: () => getContentsCount(searchParams),
-    staleTime: 5 * 1000 * 60,
-    gcTime: 30 * 1000 * 60,
-  });
-
   const {
     data: shuffledContentsData,
     fetchNextPage,
@@ -75,9 +52,10 @@ export default function ContentSlider() {
   } = useInfiniteQuery({
     queryKey: ["shuffledContents", searchParams],
     queryFn: ({ pageParam }) => getShuffledContents(pageParam, searchParams),
-    initialPageParam: getRandomNumber([]),
+    initialPageParam: getRandomNumber([], contentsCountData),
+    initialData: initialData,
     getNextPageParam: (_, __, ___, allPageParams) => {
-      return getRandomNumber(allPageParams);
+      return getRandomNumber(allPageParams, contentsCountData);
     },
     enabled: contentsCountData !== undefined,
     staleTime: 5 * 1000 * 60,
