e85e16e7ca361a8d4e64f8628f5079fedb129427
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-09-25 20:14:47 +0900

    feat: 설정창과 설정 버튼 연결

--- src/app/_components/TobTab.tsx
+++ src/app/_components/TobTab.tsx
@@ -3,13 +3,16 @@ import styles from "./TobTab.module.css";
 import logo from "@/../public/assets/DevaNiceDay.svg";
 import CategoryButton from "./CategoryButton";
 import setting from "@/../public/assets/setting.svg";
+import Link from "next/link";
 export default function TobTab() {
   return (
     <div className={styles.container}>
       <Image src={logo} priority={true} alt="logo" className={styles.logo} />
       <div className={styles.rightContainer}>
         <CategoryButton />
-        <Image src={setting} priority={true} alt="setting" />
+        <Link href={"/setting"} className={styles.settingBtn}>
+          <Image src={setting} priority={true} alt="setting" />
+        </Link>
       </div>
     </div>
   );
