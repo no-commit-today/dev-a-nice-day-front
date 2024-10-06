8d4f4f3ce616d578b1b2c34000fe9b47c73f9423
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-10-06 17:56:06 +0900

    feat: 깃허브 토큰으로 엑세스 토큰 받아오기

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,17 @@
 import Home from "./_components/Home";
 import CheckLocalStorage from "./_components/CheckLocalStorage";
+import { BASE_URL, getGitHubToken, getToken } from "../_utils/api";
 
-export default function Page({
+export default async function Page({
   searchParams,
 }: {
-  searchParams: { categories: string[]; id?: string };
+  searchParams: { categories: string[]; id?: string; code?: string };
 }) {
+  const { code } = searchParams;
+
+  const gitTokenData = await getGitHubToken(code);
+  const tokenData = await getToken(gitTokenData?.accessToken);
+
   return (
     <>
       <Home searchParams={searchParams} />
