0007942eb3db03a38dbeaa0b69bc508a747d0e45
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-11-14 20:59:33 +0900

    feat: metadataBase URL 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Suspense } from "react";
 import OgImage from "@/../public/assets/OpenGraph Image.png";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://devniceday.com"),
   title: "Dev a Nice Day | 한눈에 보는 IT 트렌드",
   description:
     "지금 알고 싶은 정보부터 더 깊은 개발 이야기까지 Dev A Nice Day에서 확인하세요!",
