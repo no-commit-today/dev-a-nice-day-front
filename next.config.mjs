aab29fd729cec80864606162302169c60550e457
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2025-04-19 18:00:30 +0900

    fix: mockdata 수정

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,30 @@ const nextConfig = {
         protocol: "https",
         hostname: "techswipe-images.s3.ap-northeast-2.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "t1.kakaocdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "static.toss.im",
+      },
+      {
+        protocol: "https",
+        hostname: "techblog.woowahan.com",
+      },
+      {
+        protocol: "https",
+        hostname: "engineering.linecorp.com",
+      },
+      {
+        protocol: "https",
+        hostname: "d1.awsstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "d2.naver.com",
+      },
     ],
   },
   output: "standalone",
